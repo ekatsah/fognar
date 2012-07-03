fbffce2f09c01d3e204d3e66bff6dfb2be83631a
Laurent Peuch <cortex@worlddomination.be>
2012-07-03 16:29:27 +0200

    pep8, blanks lines

--- profile/models.py
+++ profile/models.py
@@ -4,6 +4,7 @@ from django.db.models.signals import post_save, post_syncdb
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Profile(models.Model):
     user = models.OneToOneField(User)
     registration = models.CharField(max_length=80)
@@ -14,6 +15,7 @@ class Profile(models.Model):
     def real_name(self):
         return self.user.first_name + " " + self.user.last_name
 
+
 class Inscription(models.Model):
     user = models.ForeignKey(User)
     section = models.CharField(max_length=80, null=True)
@@ -22,12 +24,15 @@ class Inscription(models.Model):
     class Meta:
         unique_together = ('user', 'section', 'year')
 
+
 # for all user creation, create profile with default value
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance, registration=0)
 
+
 post_save.connect(create_user_profile, sender=User)
+
 
 # hack to prevent ./manage syncdb to ask for a superusers
 from django.contrib.auth.management import create_superuser
