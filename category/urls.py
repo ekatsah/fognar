518fedcb527b0b1adf43554818f4e4e4fe35592f
steak <github@hastake.com>
2012-07-04 23:46:56 +0200

    add categories for items

---
+++ category/urls.py
@@ -0,0 +1,10 @@
+# Copyright 2012, RespLab. All rights reserved.
+
+from django.conf.urls.defaults import patterns, url
+from config.authentification import stop_anon
+from views import get_category
+
+urlpatterns = patterns('',
+    url(r'^get/(?P<object_id>[^/]+)$', stop_anon(get_category), 
+        name='category_get'),
+)
