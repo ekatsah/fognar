62295ee8c29c5aadb98af333ee0d7bf47bce6a4d
steak <github@hastake.com>
2012-07-05 09:10:15 +0200

    fix header

--- group/models.py
+++ group/models.py
@@ -1,9 +1,4 @@
-# Copyright 2011, hast. All rights reserved.
-#
-# This program is free software: you can redistribute it and/or modify it
-# under the terms of the GNU Affero General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or (at
-# your option) any later version.
+# Copyright 2012, RespLab. All rights reserved.
 
 from django.contrib.auth.models import User
 from django.db import models
