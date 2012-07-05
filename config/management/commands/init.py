1f1d794aa2d6021d80d19bb510daab7f0539f80f
steak <github@hastake.com>
2012-07-05 21:06:41 +0200

    add more initial data

--- config/management/commands/init.py
+++ config/management/commands/init.py
@@ -2,7 +2,9 @@ from django.core.management.base import BaseCommand
 from django.contrib.auth.models import User
 from application.models import AppUsing
 from getpass import getpass, getuser
-
+from category.models import Category, CategoryItem
+from group.models import Group
+from course.models import Course
 
 class Command(BaseCommand):
     help = 'Initialize fognar for developpment'
@@ -26,6 +28,40 @@ class Command(BaseCommand):
             shortcuts: [ 
                 {app: 'course', slug: 'info-f-666'},
                 {app: 'course', slug: 'info-f-777'},
+                {app: 'course', slug: 'info-f-888'},
+                {app: 'course', slug: 'info-f-999'},
+                {app: 'group', slug: 'ACE'},
+                {app: 'group', slug: 'CI'},
+                
             ],
-            size: 32,
         }""")
+
+        c1 = Course.objects.create(slug='info-f-666', name='Hell Informatique',
+                                   description='Hell Computer Science course')
+        c2 = Course.objects.create(slug='info-f-777', name='Over Math',
+                                   description='New math course based on fuzzy axiomes')
+        c3 = Course.objects.create(slug='info-f-888', name='AlgoSimplex',
+                                   description='Les simplex dans tout leurs états')
+        c4 = Course.objects.create(slug='info-f-999', name='Support Vector Machines',
+                                   description='Neural Networks are outdated, use SVM!')
+
+        g1 = Group.objects.create(slug='CI', name='Cercle Informatique',
+                                  description='Cercle des etudiants en info \o/')
+        g2 = Group.objects.create(slug='ACE', name='Association des Cercles Etudiants',
+                                  description='Youplaboom')
+
+        cat0 = Category.objects.create(name='Faculty', description='Root node w/ category')
+        cat1 = Category.objects.create(name='Sciences', description='Fac de sciences')
+        cat2 = Category.objects.create(name='Polytech', description='Polytech')
+        cat3 = Category.objects.create(name='Informatique', description='Section INFO')
+
+        cat0.holds.add(cat1)
+        cat0.holds.add(cat2)
+        cat1.holds.add(cat3)
+
+        CategoryItem.objects.create(category=cat3, thing=c1, priority=1)
+        CategoryItem.objects.create(category=cat3, thing=c2, priority=2)
+        CategoryItem.objects.create(category=cat3, thing=c4, priority=3)
+        CategoryItem.objects.create(category=cat3, thing=g1, priority=3)
+        CategoryItem.objects.create(category=cat0, thing=g2, priority=1)
+        CategoryItem.objects.create(category=cat2, thing=c3, priority=1)
