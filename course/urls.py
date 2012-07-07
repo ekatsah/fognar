8f84da0b2737b15e8e5e2df11eae980e845e8db5
steak <github@hastake.com>
2012-07-07 15:44:55 +0200

    merge with ben

--- course/urls.py
+++ course/urls.py
@@ -2,16 +2,8 @@
 
 from django.conf.urls.defaults import patterns, url
 from config.authentification import stop_anon
-from config.json import json_send, json_list
-from course.models import Course
-from course.views import get_course
 
 urlpatterns = patterns('',
-    url(r'^all$', stop_anon(json_send(json_list)), 
-        {'queryset': Course.objects.all,
-         'fields': ['id', 'slug', 'name', 'description']},
+    url(r'^all$', stop_anon(), 
         name='course_all'),
-
-    url(r'^get/(?P<slug>[^/]+)$', stop_anon(get_course),
-        name='course_get'),
 )
