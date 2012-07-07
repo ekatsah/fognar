5fd9ff633d3356fec49c072ffd074df11a1546f7
steak <github@hastake.com>
2012-07-07 12:52:13 +0200

    add profile bone view

--- profile/views.py
+++ profile/views.py
@@ -1,11 +1,9 @@
 # Copyright 2012, Cercle Informatique. All rights reserved.
 
-from config.json import json_object, json_send
 from djangbone.views import BackboneAPIView
 from profile.models import Profile
-from django.contrib.auth.models import User
 
 
 class profile_bone(BackboneAPIView):
-    base_queryset = User.objects.all()
-    serialize_fields = ('
+    base_queryset = Profile.objects.all()
+    serialize_fields = ('id', 'name')
