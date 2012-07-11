4a8b6e81351e943bee9bf5564ec1bc396fbc413b
Benjamin <vanmelle.benjamin@gmail.com>
2012-07-11 14:57:54 +0200

    supporting post request to edit informations abbout wiki

--- course/views.py
+++ course/views.py
@@ -1,8 +1,10 @@
 # Copyright 2012, Cercle Informatique. All rights reserved.
 
+from config.json import json_send
 from djangbone.views import BackboneAPIView
 from course.models import Course, CourseInfo
-from course.forms import saveInfos
+from course.forms import courseF,courseInfosF
+from json import loads,dumps
 
 class course_bone(BackboneAPIView):
     base_queryset = Course.objects.all()
@@ -11,12 +13,14 @@ class course_bone(BackboneAPIView):
 class wiki_bone(BackboneAPIView):
     base_queryset = CourseInfo.objects.all()
     serialize_fields = ('id', 'user','user__name', 'infos', 'date', 'prev')
-    edit_form_class = saveInfos    # Used for PUT requests
     
+    @json_send
     def dispatch(self, request, *args, **kwargs):
-        if request.method=='PUT':
-            request.POST.rawdata
-            return super(wiki_bone, self).dispatch(request, *args, **kwargs)
+        if request.method=='POST':
+            form = loads(request.raw_post_data)
+            courseInfo = CourseInfo.objects.create(prev=CourseInfo.objects.get(pk=form['id']), user=request.user.get_profile(), infos=dumps(form['infos']))
+            Course.objects.filter(id=1).update(infos=courseInfo)
+            return 'OK'
         elif request.method=='GET':    
             return super(wiki_bone, self).dispatch(request, *args, **kwargs)
 
