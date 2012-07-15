8cefeab4cfd632c57b6a1fb64f55f77f466a32a1
steak <github@hastake.com>
2012-07-15 22:33:14 +0200

    remove debug msg

--- static/scripts/fognar.js
+++ static/scripts/fognar.js
@@ -6,7 +6,6 @@ var collections = {};
 var cache = {};
 
 Handlebars.registerHelper('get_name', function(item, options) {
-    console.log(item);
     if (item.app == 'course')
         return cache.course.get_or_fetch(item.id).get('name');
     else
