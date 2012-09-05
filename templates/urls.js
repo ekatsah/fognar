e046776f26feaa52a64c9f55cd095033325d37f3
Laurent Peuch <cortex@worlddomination.be>
2012-09-05 04:19:33 +0200

    [enh] parameters check for urls.js

--- templates/urls.js
+++ templates/urls.js
@@ -20,28 +20,34 @@ for url in urls:
 {% endcomment %} */
 
 urls = {
+    _check_arguments: function(name) {
+        for (i in arguments) {
+            if (arguments[i] == undefined)
+            throw "Error: didn't get enough arguments to construct the url";
+        }
+    },
     'application': 'zoidberg',
-    'auth_entry': function(a) { return 'auth/' + a; },
+    'auth_entry': function(a) { this._check_arguments("auth_entry", a); return 'auth/' + a; },
     'course_bone': 'course/',
-    'course_bone_id': function(a) { return 'course/' + a; },
+    'course_bone_id': function(a) { this._check_arguments("course_bone_id", a); return 'course/' + a; },
     'document_bone': 'document/d/',
-    'document_bone_id': function(a) { return 'document/d/' + a; },
-    'document_bone_type_id': function(a, b) { return 'document/r/' + a + '/' + b; },
-    'document_page': function(a) { return 'document/p/' + a; },
-    'document_rate': function(a) { return 'document/rate/' + a; },
+    'document_bone_id': function(a) { this._check_arguments("document_bone_id", a); return 'document/d/' + a; },
+    'document_bone_type_id': function(a, b) { this._check_arguments("document_bone_type_id", a, b); return 'document/r/' + a + '/' + b; },
+    'document_page': function(a) { this._check_arguments("document_page", a); return 'document/p/' + a; },
+    'document_rate': function(a) { this._check_arguments("document_rate", a); return 'document/rate/' + a; },
     'document_upload_file': 'document/upload_file',
     'document_upload_http': 'document/upload_http',
     'index': '',
     'logout': 'logout',
-    'message_bone': function(a) { return 'msg/m/' + a; },
-    'message_bone_id': function(a, b) { return 'msg/m/' + a + '/' + b; },
-    'profile_bone_id': function(a) { return 'profile/' + a; },
+    'message_bone': function(a) { this._check_arguments("message_bone", a); return 'msg/m/' + a; },
+    'message_bone_id': function(a, b) { this._check_arguments("message_bone_id", a, b); return 'msg/m/' + a + '/' + b; },
+    'profile_bone_id': function(a) { this._check_arguments("profile_bone_id", a); return 'profile/' + a; },
     'syslogin': 'syslogin',
 
-    'wiki_bone_id': function (id) { return 'course/wiki/' + id; },
+    'wiki_bone_id': function (id) { this._check_arguments("wiki_bone_id", id); return 'course/wiki/' + id; },
     'wiki_bone_save': 'course/wikisave',
 
     'thread_bone': 'msg/t/',
-    'thread_bone_id': function(a) { return 'msg/t/' + a; },
-    'thread_bone_type_id': function(a, b) { return 'msg/r/' + a + '/' + b; },
+    'thread_bone_id': function(a) { this._check_arguments("thread_bone_id", a); return 'msg/t/' + a; },
+    'thread_bone_type_id': function(a, b) { this._check_arguments("thread_bone_type_id", a, b); return 'msg/r/' + a + '/' + b; },
 };
