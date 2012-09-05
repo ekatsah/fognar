679b6eafca2cbfa7f727f5bb1a765fc682288ed7
Laurent Peuch <cortex@worlddomination.be>
2012-09-05 07:02:03 +0200

    [enh] add a created date to Thread and Message

--- message/models.py
+++ message/models.py
@@ -13,6 +13,7 @@ class Thread(models.Model):
     refer_oid = models.PositiveIntegerField()
     refer_content = models.ForeignKey(ContentType)
     referer = GenericForeignKey('refer_content', 'refer_oid')
+    created = models.DateTimeField(auto_now_add=True, editable=False)
 
 
 class Message(models.Model):
@@ -21,3 +22,4 @@ class Message(models.Model):
     text = models.TextField()
     date = models.DateTimeField(default=datetime.now)
     reference = models.ForeignKey("self", null=True)
+    created = models.DateTimeField(auto_now_add=True, editable=False)
