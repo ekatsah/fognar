0a0654e0716e2dc5bbf233da8caad5309106bd9f
Laurent Peuch <cortex@worlddomination.be>
2012-09-06 04:23:33 +0200

    [mod] refactoring, meaningful variable name

--- config/utils.py
+++ config/utils.py
@@ -3,10 +3,10 @@ from group.models import Group
 from django.shortcuts import get_object_or_404
 
 
-def get_context(ctype, context):
-    if ctype == 'course':
+def get_context(content_type, context):
+    if content_type == 'course':
         return get_object_or_404(Course, id=context)
-    elif ctype == 'group':
+    elif content_type == 'group':
         return get_object_or_404(Group, id=context)
     else:
         # Force 404
