f62744799bb5e1adee222a7e03522456d25981f8
Laurent Peuch <cortex@worlddomination.be>
2012-09-06 02:15:42 +0200

    [enh] beeeh gigantic modification to make this fucking thread section working (no new threads for the moment)

--- static/scripts/message.js
+++ static/scripts/message.js
@@ -1,16 +1,92 @@
 // Copyright 2012, Cercle Informatique. All rights reserved.
 
-models.thread = Backbone.Model.extend({
-    initialize: function(params) {
-//        this.type = params.type;
-//        this.context = params.context;
-    },
-
+models.thread = Backbone.RelationalModel.extend({
     url: urls['thread_bone'],
+    relations: [{
+        type: Backbone.HasMany,
+        key: 'messages',
+        relatedModel: 'models.message',
+        collectionType: 'collections.messages',
+    },{
+        type: Backbone.HasOne,
+        key: 'user',
+        relatedModel: 'models.user'
+    }],
+    initialize: function(params) {
+        console.log("New thread model with id " + params.id);
+    },
 });
 
-models.message = Backbone.Model.extend({
-    urlRoot: urls['message_bone'],
+models.message = Backbone.RelationalModel.extend({
+    relations: [{
+        type: Backbone.HasOne,
+        key: 'user',
+        relatedModel: 'models.user',
+    }],
+    //url: function() { this.attributes.id ? 'msg/m/' + this.attributes.id : '/msg/m/' },
+    url: '/msg/m/',
+    initialize: function(params) {
+        console.log("New message model with id " + params.id);
+        console.log(this.url);
+        this.wrap_save_for_djangbone();
+    },
+    wrap_save_for_djangbone: function() {
+        this._save = this.save;
+        this.save = function() {
+            this.relations[0].includeInJSON = Backbone.Model.prototype.idAttribute;
+            this._save();
+            this.relations[0].includeInJSON = true;
+        }
+    },
+});
+
+collections.messages = Backbone.Collection.extend({
+    model: models.messages,
+    initialize: function(params) {
+        console.log("New messages collection");
+    },
+});
+
+applications.submit_new_message = Backbone.View.extend({
+    events: {
+        "submit": "add_new_message",
+    },
+    initialize: function(params) {
+        console.log("New submit_new_message view");
+    },
+    add_new_message: function(event) {
+        var text = this.$el.find("textarea").val();
+        console.log("add a new message with text: " + text);
+        event.preventDefault();
+        this.$el.find("textarea").val("");
+        var new_message = new models.message({
+            text: text,
+            thread: this.model.id,
+            user: window.profile.id,
+            reference: null,
+        });
+        this.model.attributes.messages.add(new_message);
+        new_message.save();
+    }
+});
+
+applications.thread_view = Backbone.View.extend({
+    initialize: function() {
+        this.model.attributes.messages.bind("change", this.render, this);
+        this.name = "moi";
+        this.render();
+    },
+    render: function() {
+        console.log("Je me render: " + this.name);
+        //console.log(JSON.stringify(this.model.toJSON()));
+        this.$el.html(templates['tpl-thread-thread']({
+            thread: this.model.toJSON(),
+        }));
+        new applications.submit_new_message({
+            el: $("#submit_new_message_" + this.model.id),
+            model: this.model,
+        });
+    }
 });
 
 applications.thread = Backbone.View.extend({
@@ -53,6 +129,12 @@ applications.thread = Backbone.View.extend({
             context: this.context.toJSON(),
             threads: this.threads.toJSON(),
         }));
+        this.threads.each(function(thread) {
+            new applications.thread_view({
+                el: $('#thread_' + thread.id),
+                model: thread,
+            })
+        });
         return this;
     },
 
