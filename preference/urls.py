470396583a6e242688f4eef6c17352438b3f1605
Laurent Peuch <cortex@worlddomination.be>
2012-09-07 11:14:12 +0200

    [enh] allow to add new shortcuts

--- preference/urls.py
+++ preference/urls.py
@@ -1,13 +1,17 @@
 # Copyright 2012, Cercle Informatique. All rights reserved.
 
-from json import dumps
+from json import dumps, loads
 
 from django.conf.urls.defaults import patterns, url
 from django.http import HttpResponse
+from django.views.decorators.http import require_POST
+from django.db.models import Max
+from django import forms
 
 from course.models import Course
 from group.models import Group
 from config.authentification import stop_anon
+from models import Shortcut
 
 
 APPS_MODELS = {
@@ -30,6 +34,29 @@ def remove_shortcut(request, id):
     return HttpResponse('success!')
 
 
+class NewShortcut(forms.Form):
+    app = forms.ChoiceField([('course', 'course')])
+    app_id = forms.IntegerField()
+
+
+@require_POST
+def add_shortcut(request):
+    form = NewShortcut(loads(request.raw_post_data))
+    if not form.is_valid():
+        return HttpResponse('<p>Bad data:</p>' + str(form.errors), status=400)
+
+    max_position = Shortcut.objects.filter(user=request.user.profile).aggregate(Max('position'))['position__max']
+    if not max_position:
+        max_position = 1
+
+    Shortcut.objects.create(user=request.user.profile,
+        app=form.cleaned_data["app"],
+        app_id=form.cleaned_data["app_id"],
+        position=max_position + 1
+    )
+    return HttpResponse('success!')
+
+
 urlpatterns = patterns('',
     url(r'^shortcuts/$',
         stop_anon(get_desktop_data),
@@ -37,5 +64,9 @@ urlpatterns = patterns('',
 
     url(r'^shortcuts/remove/(?P<id>\d+)$',
         stop_anon(remove_shortcut),
+        name="user_preference"),
+
+    url(r'^shortcuts/add/$',
+        stop_anon(add_shortcut),
         name="user_preference"),
 )
--- static/scripts/market.js
+++ static/scripts/market.js
@@ -4,6 +4,24 @@ models.shortcut = Backbone.Model.extend({
     url: "/preference/shortcuts/add/",
 });
 
+applications.market_course = Backbone.View.extend({
+    events: {
+        "click .add-course": "add_course",
+    },
+
+    add_course: function(event) {
+        event.preventDefault();
+        console.log("Add course");
+        var self = this;
+        new_shortcut = new models.shortcut({
+            app: "course",
+            app_id: self.model.id,
+        });
+        new_shortcut.save();
+        this.$el.remove();
+    }
+});
+
 applications.market = Backbone.View.extend({
     initialize: function() {
         _.bindAll(this, 'render');
@@ -23,6 +41,12 @@ applications.market = Backbone.View.extend({
             course: this.collection.toJSON(),
         }));
 
+        this.collection.each(function(i) {
+            new applications.market_course({
+                el: $("#course_" + i.id),
+                model: i,
+            })
+        })
     },
 
     close: function() {},
