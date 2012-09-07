e08415fee1947ede2c4a4aae2cbe2885948ecdd2
Laurent Peuch <cortex@worlddomination.be>
2012-09-07 07:51:51 +0200

    [mod] remove debug

--- static/scripts/desktop.js
+++ static/scripts/desktop.js
@@ -31,7 +31,6 @@ applications.desktop = Backbone.View.extend({
     },
 
     render: function() {
-        console.log(this.collection.toJSON());
         this.$el.html(templates['tpl-desktop']({
             shortcuts: this.collection.toJSON(),
         }));
